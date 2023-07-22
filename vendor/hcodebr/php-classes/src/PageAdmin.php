8cdaf9c035260e06571b681e7588c6e8e2e00089
Aldo Michelato <aldomichelato@gmail.com>
2023-07-22 20:50:54 -0300

    Criamos o admin

--- vendor/hcodebr/php-classes/src/PageAdmin.php
+++ vendor/hcodebr/php-classes/src/PageAdmin.php
@@ -4,10 +4,10 @@ namespace Hcode;
 
 class PageAdmin extends Page{
 
-    public function __construct($opts = array(), $tpl_dir = "/views/admin")
+    public function __construct($opts = array(), $tpl_dir = "/views/admin/")
     {
         
-        parent::__construct($opts, $tpl_dir);
+        parent::__construct($opts, $tpl_dir); //metodo parent faz referencia a classe pai que foi herdadam no caso a Page
     
     }
 
