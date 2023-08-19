4537bec1002dae702d91eb2083ca56a6bcdc356b
Aldo Michelato <aldomichelato@gmail.com>
2023-08-19 18:35:38 -0300

    tela de login

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new \Slim\Slim();
 
@@ -20,9 +21,40 @@ $app->get('/', function() {
 
 $app->get('/admin', function() {
     
+    User::VerifyLogin();
+
 	$page = new PageAdmin();
 
 	$page->setTpl("index");
+
+});
+
+$app->get('/admin/login', function() {
+    
+	$page = new PageAdmin([ //na pagina de login, é necessário verificar se aaparece o cabeçalho e o rodapé, pois geralmente nesse caso NAO DEVEM APARECER
+		"header"=>false, //desabilitar o header padrão
+		"footer"=>false  //e também o footer padrão
+	]);
+
+	$page->setTpl("login"); //chama o template login
+
+});
+
+$app->post('/admin/login', function(){  //rota post
+    
+	User::login($_POST["login"], $_POST["password"]);  //metodo estatico chamado login que recebe o post de login e password do formulario
+	
+	header("Location: /admin"); //redireciona para a administração
+	exit;
+
+});
+
+$app->get('/admin/logout', function() {
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
 
 });
 
--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -9,13 +9,15 @@ class Page{
     private $tpl; //private outras classes nao tem acesso
     private $options = [];
     private $defaults = [
+        "header"=>true, //cabeçalho é true (vai aparecer)
+        "footer"=>true, //rodapé também
         "data"=>[]
 
     ];
 
     public function __construct($opts = array(), $tpl_dir = "/views/"){
         
-        $this->options = array_merge($this->defaults, $opts); //
+        $this->options = array_merge($this->defaults, $opts); // array_merge - Combina um ou mais arrays
 
         $config = array(
             "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
@@ -29,7 +31,7 @@ class Page{
 
         $this->setData($this->options["data"]);
 
-        $this->tpl->draw("header"); //chama na tela
+        if($this->options["header"] === true) $this->tpl->draw("header"); //chama na tela
 
     }
 
@@ -51,7 +53,7 @@ class Page{
 
     public function __destruct(){
         
-        $this->tpl->draw("footer");
+        if($this->options["footer"] === true) $this->tpl->draw("footer"); //chama na tela
     }
 
 
