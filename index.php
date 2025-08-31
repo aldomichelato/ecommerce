1c8551cc0e6dfa2eab765cdb7b14512796a1a278
Aldo Neto <aldomichelato@gmail.com>
2025-08-31 12:11:21 -0300

    auto load br classes

--- index.php
+++ index.php
@@ -7,6 +7,11 @@ $app = new \Slim\Slim();
 $app->config('debug', true);
 
 $app->get('/', function() {
+
+	$sql = new Hcode\DB\Sql();
+	$results = $sql->selecT("SELECT * FROM TB_USERS");
+
+	ECHO json_encode($results);
     
 	echo "OK talarica";
 
